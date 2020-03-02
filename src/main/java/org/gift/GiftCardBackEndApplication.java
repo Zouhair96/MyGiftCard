eeddd1bd0881ec6f62c2a4ba34f6d1b460f4b6a4
Zouhair96 <zouhair.benali96@gmail.com>
2020-03-02 11:05:19 +0000

    initial commit

---
+++ src/main/java/org/gift/GiftCardBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.gift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftCardBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftCardBackEndApplication.class, args);
+	}
+
+}
