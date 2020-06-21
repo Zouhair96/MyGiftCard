e1d76ab6edca0083bcfad9fed3e2d38817e44f20
Zouhair96 <zouhair.benali96@gmail.com>
2020-06-21 17:59:56 +0100

    add test file

---
+++ src/test/java/org/gift/TestPanier.java
@@ -0,0 +1,79 @@
+package org.gift;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class TestPanier {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+	  String projectPath = System.getProperty("user.dir");
+	  System.setProperty("webdriver.chrome.driver",projectPath + "/driver/chromedriver.exe");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testPanier() throws Exception {
+    driver.get("http://localhost:4200/");
+    driver.findElement(By.xpath("//a/p")).click();
+    driver.findElement(By.xpath("(//button[@type='submit'])[2]")).click();
+    driver.findElement(By.xpath("//div[3]/div")).click();
+    acceptNextAlert = true;
+    driver.findElement(By.xpath("//button/span")).click();
+    assertEquals(closeAlertAndGetItsText(), "T es sur !");
+    driver.findElement(By.xpath("//div[2]/div/button")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
